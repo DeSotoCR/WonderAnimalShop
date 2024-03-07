d73d95535444396dbb6ea85e9549ef84287d154f
fioportu <portuguezfiore@gmail.com>
2024-03-07 17:49:59 -0600

    fiorella

--- src/main/java/com/WonderAnimalShop/Domain/Usuario.java
+++ src/main/java/com/WonderAnimalShop/Domain/Usuario.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.WonderAnimalShop.Domain;
+/*Categoria Domain Java*/
+package com.WonderAnimalShop.domain;
 
-/**
- *
- * @author NightFox
- */
-public class Usuario {
-    
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuario")
+
+public class Usuario implements Serializable { //serializacion porque se va almacenar ciertos datos en el disco
+
+    private static final long serialVersionUID = 1L; //para poder hacer el ciclo de la sumatoria de la categoria.
+
+    @Id //id categoria es la llave de la tabla categoria. 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Los valores generados que estrategia usan, identico a la BD 
+    @Column(name = "id_categoria") //decir cual es el nombre en la base de datos. Se hace la asociación 
+    private int idUsuario;
+    private String correo;
+    private String contraseña;
+
+    public Usuario() {
+    }
+
+    public Usuario(String correo, String contraseña) {
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
 }
